72c2b16178ec87e7bf81ab917fb6a6c14f9756d1
Your Name <you@example.com>
2017-03-19 17:24:02 +0900

    fix major bugs

--- keras_rnn_tenioha.py
+++ keras_rnn_tenioha.py
@@ -25,42 +25,31 @@ from itertools import cycle as Cycle
 def build_model(maxlen=None, out_dim=None, in_dim=256):
   print('Build model...')
   model = Sequential()
-  model.add(GRU(128*5, return_sequences=True, input_shape=(maxlen, in_dim)))
+  model.add(GRU(128*20, return_sequences=False, input_shape=(maxlen, in_dim)))
   model.add(BN())
-  model.add(GN(0.2))
-  model.add(GRU(128*5, return_sequences=False, input_shape=(maxlen, in_dim)))
-  #model.add(BN())
-  model.add(GN(0.2))
   model.add(Dense(out_dim))
   model.add(Activation('linear'))
   model.add(Activation('sigmoid'))
+  #model.add(Activation('softmax'))
   optimizer = Adam()
   model.compile(loss='binary_crossentropy', optimizer=optimizer) 
   return model
 
-def sample(preds, temperature=1.0):
-  preds = np.asarray(preds).astype('float64')
-  preds = np.log(preds) / temperature
-  exp_preds = np.exp(preds)
-  preds = exp_preds / np.sum(exp_preds)
-  probas = np.random.multinomial(1, preds, 1)
-  return np.argmax(probas)
-
-def dynast(preds, temperature=1.0):
-  preds = np.asarray(preds).astype('float64')
-  preds = np.log(preds) / temperature
-  exp_preds = np.exp(preds)
-  preds = exp_preds / np.sum(exp_preds)
-  return np.argmax(preds)
 
 TENIWOHA = set( list( filter(lambda x:x!="", """が,の,を,に,へ,と,で,や,の,に,と,や,か,は,も,ば,と,が,し,て,か,な,ぞ,わ,よ""".split(',') ) ) )
-TENIWOHA_IDX = { x:i for i,x in enumerate( filter(lambda x:x!="", """が,の,を,に,へ,と,で,や,の,に,と,や,か,は,も,ば,と,が,し,て,か,な,ぞ,わ,よ""".split(',') ) )  }
+TENIWOHA_IDX = { }
+
+for i, x in enumerate(list("がのをにへとでやかはもばしてなぞわよ")):
+  TENIWOHA_IDX[x] = i
+TENIWOHA_INV = { i:x for x,i in TENIWOHA_IDX.items() }
+
 def preexe():
   print(TENIWOHA)
   dataset = []
   term_vec = pickle.loads(open('term_vec.pkl', 'rb').read())
   with open('dump.news.wakati', 'r') as f:
-    for line in f:
+    for fi, line in enumerate(f):
+      if fi > 10000: break
       terms = line.split()
       for cur in range(10, len(terms) - 10, 1):
         if terms[cur] in TENIWOHA:
@@ -68,7 +57,8 @@ def preexe():
              head = list(map(lambda x:term_vec[x], terms[cur-10:cur]))
              ans  = terms[cur]
              tail = list(map(lambda x:term_vec[x], terms[cur+1:cur+10]))
-             dataset.append( (head, ans, tail) )
+             pure_text = terms[cur-10:cur+10]
+             dataset.append( (head, TENIWOHA_IDX[ans], tail, pure_text) )
            except KeyError as e:
              pass
   print("all data set is %d"%len(dataset))
@@ -77,53 +67,63 @@ def preexe():
 def train():
   print("importing data from algebra...")
   datasets = pickle.loads(open('dataset.pkl', 'rb').read())
-  model    = build_model(maxlen=len(datasets), in_dim=256, out_dim=len(TENIWOHA))
   sentences = []
   answers   = []
-  for dbi, series in enumerate(datasets[:2048]):
-    head, ans, tail = series 
+  to_use = datasets[:50000]
+  random.shuffle(to_use)
+  for dbi, series in enumerate(to_use):
+    # 64GByteで最大80万データ・セットくらいまで行ける
+    head, ans, tail, pure_text = series 
     head.extend(tail)
     sentences.append(np.array(head))
-    answers.append(TENIWOHA_IDX[ans])
+    answers.append(ans)
   print('nb sequences:', len(sentences))
 
   print('Vectorization...')
-  X = np.zeros((len(sentences), len(sentences[0]), 256), dtype=np.bool)
-  y = np.zeros((len(sentences), len(TENIWOHA_IDX)), dtype=np.bool)
+  X = np.zeros((len(sentences), len(sentences[0]), 256), dtype=np.float64)
+  y = np.zeros((len(sentences), len(TENIWOHA_IDX)), dtype=np.int)
   for i, sentence in enumerate(sentences):
+    if i%10000 == 0:
+      print("building training vector... iter %d"%i)
     for t, vec in enumerate(sentence):
-      #print(vec)
-      #print(len(vec))
       X[i, t, :] = vec
-    y[i, :] = answers[i]
-  sys.exit() 
-  for iteration in range(1, 4):
+    y[i, answers[i]] = 1
+  model    = build_model(maxlen=len(sentences[0]), in_dim=256, out_dim=len(TENIWOHA))
+  for iteration in range(1, 41):
     print()
     print('-' * 50)
     print('Iteration', iteration)
     model.fit(X, y, batch_size=128, nb_epoch=1)
-    MODEL_NAME = "./models/snapshot.%09d.model"%(ci)
+    MODEL_NAME = "./models/snapshot.%09d.model"%(iteration)
     model.save(MODEL_NAME)
 
 def pred():
-  model  = load_model(sorted(glob.glob('./*.model'))[-1] )
-  for name in glob.glob('./samples/*'):
-    text = open(name).read() * 10
-    #print(text)
-    tag_index = pickle.loads(open('tag_index.pkl', 'rb').read())
-    term_vec = pickle.loads(open('term_vec.pkl', 'rb').read())
-    m = MeCab.Tagger('-Owakati')
-    terms = m.parse(text).split()
-    contexts = []
-    for term in terms[:200]:
-      try:
-        contexts.append(term_vec[term]) 
-      except KeyError as e:
-        contexts.append(term_vec["ダミー"])
-    result = model.predict(np.array([contexts]))
-    result = {i:w for i,w in enumerate(result.tolist()[0])}
-    for tag, index in sorted(tag_index.items(), key=lambda x:result[x[1]]):
-      print(name, tag, result[index], index)
+  model_type = sorted(glob.glob('./models/snapshot.*.model'))[-1]
+  print("start to loading dataset")
+  datasets = pickle.loads(open('dataset.pkl', 'rb').read())
+  print("model type is %s"%model_type)
+  model  = load_model(model_type)
+  sentences = []
+  answers   = []
+  texts     = []
+  for dbi, series in enumerate(datasets[0:10]):
+    head, ans, tail, pure_text = series 
+    head.extend(tail)
+    sentences.append(np.array(head))
+    answers.append(ans)
+    texts.append(pure_text)
+  X = np.zeros((len(sentences), len(sentences[0]), 256), dtype=np.float64)
+  y = np.zeros((len(sentences), len(TENIWOHA_IDX)), dtype=np.int)
+  for i, sentence in enumerate(sentences):
+    if i%10000 == 0:
+      print("building training vector... iter %d"%i)
+    for t, vec in enumerate(sentence):
+      X[i, t, :] = vec
+  results = model.predict(X)
+  for sent, text, result in zip(sentences, texts, results):
+    print([(i,t) for i,t in enumerate(text)])
+    for i,f in sorted([(i,f) for i,f in enumerate(result.tolist())], key=lambda x:x[1]*-1):
+      print(TENIWOHA_INV[i], f)
 
 def main():
   if '--preexe' in sys.argv:
